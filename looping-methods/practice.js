215e1fb7a52d38fd186e9cf330d58af754f9e306
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2023-12-26 22:37:05 -0500

    Practicing while and do... while

--- looping-methods/practice.js
+++ looping-methods/practice.js
@@ -9,9 +9,25 @@ const lunarPhases = [
     'Waning Crescent'
 ];
 
+// Function to get the image source based on the moon phase
+function getImageSrc(moonPhase) {
+    return `./img/${moonPhase.toLowerCase().replace(' ', '-')}.png`;
+}
+
+//Function to create loop heading
+function createHeading(text) {
+    const heading = document.createElement('h2');
+    heading.textContent = text;
+    document.body.appendChild(heading);
+
+}
+
+let forHeading = createHeading('for Loop');
+
+// for Loop: The traditional for loop is one of the most widely used. It repeats a block of code a certain number of times based on a counter variable.
 for (let i = 0; i < lunarPhases.length; i++) {
     const moonPhase = lunarPhases[i];
-    const imageSrc = getImageSrc(moonPhase); // Replace this with your own function to get the image source based on the moon phase
+    const imageSrc = getImageSrc(moonPhase); // function to get the image source based on the moon phase
 
     const img = document.createElement('img');
     img.src = imageSrc;
@@ -20,7 +36,36 @@ for (let i = 0; i < lunarPhases.length; i++) {
     document.body.appendChild(img);
 }
 
-// Replace this with your own function to get the image source based on the moon phase
-function getImageSrc(moonPhase) {
-    return `./img/${moonPhase.toLowerCase().replace(' ', '-')}.png`;
+let whileHeading = createHeading('while Loop');
+
+// while Loop: The while loop continues executing its code block as long as the specified condition is true.
+let j = 0;
+while (j < lunarPhases.length) {
+    const moonPhase = lunarPhases[j];
+    const imageSrc = getImageSrc(moonPhase); // function to get the image source based on the moon phase
+
+    const img = document.createElement('img');
+    img.src = imageSrc;
+    img.alt = moonPhase;
+
+    document.body.appendChild(img);
+
+    j++;
 }
+
+let doWhileHeading = createHeading('do...while Loop');
+
+// do...while Loop: Similar to the while loop, but it runs at least once because the condition is checked after the code block is executed.
+let k = 0;
+do {
+    const moonPhase = lunarPhases[k];
+    const imageSrc = getImageSrc(moonPhase); // function to get the image source based on the moon phase
+
+    const img = document.createElement('img');
+    img.src = imageSrc;
+    img.alt = moonPhase;
+
+    document.body.appendChild(img);
+
+    k++;
+} while (k < lunarPhases.length);
