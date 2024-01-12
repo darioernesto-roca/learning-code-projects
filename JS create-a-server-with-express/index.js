391d86bb7b699bb74ad4e1330c40feb56e4c65d3
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-01-11 23:24:25 -0500

    Update Express

--- JS create-a-server-with-express/index.js
+++ JS create-a-server-with-express/index.js
@@ -5,4 +5,38 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+express.static('public');
+
+// Error handling
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    // Forward error to the next middleware
+    next(error);
+});
+
+// Error handling
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.send(error.message);
+});
+
+// Middleware
+app.use((req, res, next) => {
+    console.log('A new request received at ' + Date.now());
+    // This function call tells that more processing is
+    // required for the current request and is in the next middleware
+    next();
+});
+
+//logging auth
+app.use((req, res, next) => {
+    console.log(req.headers.authorization);
+    next();
+});
+
+
+
+
 app.listen(3000, () => console.log('🌎 Server running on port http://localhost:3000'));
+
