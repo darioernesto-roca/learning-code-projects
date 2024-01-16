a0661462c75468706ff54ee16cf58cee59ed8fc0
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-01-15 22:43:02 -0500

    Practicing Promises, then and catch

--- JS commonjs-vs-esmodules/index.js
+++ JS commonjs-vs-esmodules/index.js
@@ -1,5 +1,7 @@
 // index.js
-import { data, method } from "./module.js";
+import { data, method, number, secuenceOfPromises } from "./module.js";
 
 console.log(data); // 42
 method(); // Hello
+console.log(number); // 43
+secuenceOfPromises(); // [ 'Promise 1 resolved', 'Promise 2 resolved', 'Promise 3 resolved' ]
--- JS commonjs-vs-esmodules/module.js
+++ JS commonjs-vs-esmodules/module.js
@@ -1,3 +1,49 @@
 // module.js
 export const data = 42;
 export const method = () => console.log("Hello");
+export const number = data + 1;
+
+export function secuenceOfPromises() {
+    const promise1 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 1 resolved');
+        }, 2000);
+    });
+
+    const promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 2 resolved');
+        }, 1500);
+    });
+
+    const promise3 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('Promise 3 rejected'));
+        }, 1000);
+    });
+
+    const promise4 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Promise 4 resolved');
+        }, 1000);
+        
+    });
+
+    promise1.then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    }).finally(() => {
+        console.log('Promise 1 completed');
+    });
+
+    Promise.all([promise1, promise2, promise3, promise4])
+    .then((values) => {
+        console.log(values);
+    }).catch((error) => {
+        console.log(error);
+    }).finally(() => {
+        console.log('All promises resolved');
+    });
+}
+
