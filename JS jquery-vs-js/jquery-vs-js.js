73a66004c27c8d1932dd44edd0e8fbec22910f4f
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-01-24 21:30:01 -0500

    JQuery vs JS

---
+++ JS jquery-vs-js/jquery-vs-js.js
@@ -0,0 +1,35 @@
+//Finding HTML Element by Id
+
+//JQuery
+myElement = $("#id01");
+
+//JavaScript
+myElement = document.getElementById("id01");
+
+
+//Finding HTML Elements by Tag Name
+
+//JQuery
+myElements = $("p");
+
+//JavaScript
+myElements = document.getElementsByTagName("p");
+
+
+//Finding HTML Elements by Class Name
+
+//JQuery
+myElements = $(".intro");
+
+//JavaScript
+myElements = document.getElementsByClassName("intro");
+
+
+//Finding HTML Elements by CSS Selectors
+
+//JQuery
+myElements = $("p.intro");
+
+//JavaScript
+myElements = document.querySelectorAll("p.intro");
+
