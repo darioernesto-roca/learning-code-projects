e7ea86d861e346edba6b9a57043f7a7f01b20e5b
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-02-09 23:33:41 -0500

    JS Operators

---
+++ JavaScript/JS Operators/operators.js
@@ -0,0 +1,19 @@
+// Division Assignment Operator is used to divide the value of the variable by the value of the right operand and then assign the result to the variable.
+let x = 10;
+x /= 5; // x = x / 5
+console.log(x); // 2
+
+// Exponentiation Assignment Operator is used to raise the value of the variable to the power of the value of the right operand and then assign the result to the variable.
+let y = 2;
+y **= 3; // y = y ** 3
+console.log(y); // 8
+
+// Remainder Assignment Operator is used to divide the value of the variable by the value of the right operand and then assign the remainder to the variable.
+let z = 10;
+z %= 3; // z = z % 3
+console.log(z); // 1
+
+// The Left Shift Assignment Operator is used to shift the value of the variable to the left by the number of bits specified by the value of the right operand and then assign the result to the variable.
+let a = -100;
+a <<= 5; // a = a << 5
+console.log(a); // -3200
