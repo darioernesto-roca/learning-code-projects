05dd3263307804a5a017c2ac3baea07194c5711e
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-02-12 23:41:53 -0500

    String methods

---
+++ JavaScript/JS string/strings-search.js
@@ -0,0 +1,31 @@
+// String Search Methods
+
+// String indexOf()
+const str = "Hello, world!";
+const index = str.indexOf("o");
+console.log(index); // Output: 4
+
+
+// String lastIndexOf()
+
+const str2   = "Hello, world!";
+const index2 = str2.lastIndexOf("o");
+console.log(index2); // Output: 8
+
+// String search()
+const str3 = "Hello, world!";
+const index3 = str3.search("o");
+console.log(index3); // Output: 4
+
+// String match()
+const str4 = "Hello, world!";
+const matches = str4.match("o");
+console.log(matches); // Output: [ 'o', index: 4, input: 'Hello, world!', groups: undefined ]
+
+// String match() with global flag
+const str5 = "Hello, world!";
+const matches2 = str5.match(/o/g);
+console.log(matches2); // Output: [ 'o', 'o' ]
+
+// What's the difference between indexoOf and search?
+// The difference between indexOf() and search() is that search() can take a regular expression as an argument.
