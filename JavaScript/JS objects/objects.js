7b0d3280002fdda85bb4179f02ef87a546710831
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-03-13 23:49:41 -0500

    Objects Property existence test, “in” operator and for..in loop

--- JavaScript/JS objects/objects.js
+++ JavaScript/JS objects/objects.js
@@ -70,3 +70,36 @@ alert(user["likes birds"]); // true
 
 // delete
 delete user["likes birds"];
+
+
+// Special operator in
+let userB = { name: "John", age: 30 };
+
+console.log( "age" in userB ); // true, user.age exists
+console.log( "blabla" in userB ); // false, user.blabla doesn't exist
+
+//Why does the in operator exist? Isn’t it enough to compare against undefined?. Well, most of the time the comparison with undefined works fine. But there’s a special case when it fails, but "in" works correctly. It’s when an object property exists, but stores undefined:
+
+let obj = {
+    test: undefined
+};
+
+console.log( obj.test ); // it's undefined, so - no such property?
+console.log( "test" in obj ); // true, the property does exist!
+
+
+// The "for…in" loop
+// To walk over all keys of an object, there exists a special form of the loop: for..in. This is a completely different thing from the for(;;) construct that we studied before. Notice all “for” constructs allow us to declare the looping variable inside the loop, like let key here. Also, we could use another variable name here instead of key. For instance, "for (let prop in obj)" is also widely used.
+
+let userC = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+};
+
+for (let key in userC) {
+    // keys
+    console.log( key );  // name, age, isAdmin
+    // values for the keys
+    console.log( userC[key] ); // John, 30, true
+}
