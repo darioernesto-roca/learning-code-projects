ec217b8087944c22245107f8c9e362eb1daf6a7d
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-03-19 21:39:23 -0500

    Creating a Night Table object, adding properties, metods, and objects inside the object

--- JavaScript/JS Training/Practice/03_07/script.js
+++ JavaScript/JS Training/Practice/03_07/script.js
@@ -7,3 +7,44 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const nightTable = {
+  name: "Night Table",
+  color: "brown",
+  material: "wood",
+  drawerNum: 2,
+  drawerOpen: false,
+  toggleDrawer: function (drawerStatus) {
+    this.drawerOpen = drawerStatus;
+  },
+  drowerContent: {
+    drawer1: [
+      {
+        name: "Key Ring",
+        color: "silver",
+        material: "metal",
+        keys: ["Front Door", "Outside Door", "Car", "Department", "Office"],
+        keyChain: "Leather",
+      },
+      "Watch",
+      "Wallet",
+      "Documents",
+    ],
+    drawer2: ["Tools", "Screwdriver", "Gripper", "Scalpel"],
+  },
+};
+
+nightTable.toggleDrawer(true);
+console.log("The nightTable object", nightTable);
+
+let query = "drawerNum";
+console.log("The drawerNum value:", nightTable[query]); // 2
+
+query = "drowerContent";
+console.log("The drowerContent value:", nightTable[query]); // {drawer1: Array(4), drawer2: Array(3)}
+
+query = "drawer1";
+console.log("The drawer1 value:", nightTable.drowerContent[query]); // (4) [{…}, "Watch", "Wallet", "Documents"]
+
+query = "keyChain";
+console.log("The keyChain value:", nightTable.drowerContent.drawer1[0][query]); // Leather
