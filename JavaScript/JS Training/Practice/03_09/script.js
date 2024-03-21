46d136b45c3df88baa3ca00574e12863646c68e7
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-03-20 21:02:17 -0500

    Practicing with object methods

--- JavaScript/JS Training/Practice/03_09/script.js
+++ JavaScript/JS Training/Practice/03_09/script.js
@@ -12,6 +12,7 @@ const backpack = {
   volume: 30,
   color: "grey",
   pocketNum: 15,
+  lidOpen: false,
   strapLength: {
     left: 26,
     right: 26,
@@ -23,4 +24,42 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  changeColor: function (newColor) {
+    this.color = newColor;
+  },
+  changeVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  changePocketNum: function (newPocketNum) {
+    this.pocketNum = newPocketNum;
+  }
 };
+
+console.log("Before: ", backpack.lidOpen); // false
+backpack.toggleLid(true);
+console.log("After toggleLid: ", backpack.lidOpen); // true
+
+console.log("Before newStrapLength: ", backpack.strapLength); // { left: 26, right: 26 }
+backpack.newStrapLength(20, 20);
+console.log("After newStrapLength: ", backpack.strapLength); // { left: 20, right: 20 }
+
+console.log("Before changeColor: ", backpack.color); // grey
+backpack.changeColor("blue");
+console.log("After changeColor: ", backpack.color); // blue
+
+console.log("Before changeVolume: ", backpack.volume); // 30
+backpack.changeVolume(50);
+console.log("After changeVolume: ", backpack.volume); // 50
+
+console.log("Before changeName: ", backpack.name); // Everyday Backpack
+backpack.changeName("New Backpack");
+console.log("After changeName: ", backpack.name); // New Backpack
+
+console.log("Before changePocketNum: ", backpack.pocketNum); // 15
+backpack.changePocketNum(20);
+console.log("After changePocketNum: ", backpack.pocketNum); // 20
+
+// Path: JavaScript/JS%20Training/Practice/03_10/script.js
