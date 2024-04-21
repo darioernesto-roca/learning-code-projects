477c1c1a504dc46372b50c8d307e6f572699f1a7
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-04-21 15:02:57 -0500

    Practicing array methods

--- JavaScript/JS Training/07_02/script.js
+++ JavaScript/JS Training/07_02/script.js
@@ -7,4 +7,14 @@ let item = "flashlight";
 
 const collection = ["Piggy", item, 5, true];
 
-console.log(collection);
+console.log(collection.length); // 4
+console.log(collection[3]); // true
+
+collection.shift(); // remove first item
+console.log(collection); // ["flashlight", 5, true]
+collection.unshift("Piggy"); // add item to the beginning
+
+collection.pop(); // remove last item
+console.log(collection); // ["Piggy", "flashlight", 5]
+collection.push(true); // add item to the end
+
