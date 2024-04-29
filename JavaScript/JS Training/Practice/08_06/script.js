ab76176d20c700c1f7869b3c066a96e2dd743452
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-04-28 21:34:01 -0500

    Create Article function

--- JavaScript/JS Training/Practice/08_06/script.js
+++ JavaScript/JS Training/Practice/08_06/script.js
@@ -5,3 +5,19 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+function findElement(selector) {
+  return document.querySelector(selector);
+}
+
+const mainElement = findElement("main");
+
+const createArticle = (data) => {
+  const article = document.createElement("article");
+  article.innerHTML = `
+    <h2>${data.title}</h2>
+    <p>${data.description}</p>
+  `;
+  return article;
+};
+
+mainElement.appendChild(createArticle({ title: "Title 1", description: "Description 1" }));
