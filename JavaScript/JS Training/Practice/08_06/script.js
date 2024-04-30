05beccf50480edb696b2413cbc6e03b24b5dc6d2
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-04-29 20:32:31 -0500

    Creating functions

--- JavaScript/JS Training/Practice/08_06/script.js
+++ JavaScript/JS Training/Practice/08_06/script.js
@@ -21,3 +21,36 @@ const createArticle = (data) => {
 };
 
 mainElement.appendChild(createArticle({ title: "Title 1", description: "Description 1" }));
+
+// Arrow function
+
+const createSidebar = (data) => {
+  const sidebar = document.createElement("aside");
+  sidebar.innerHTML = `
+    <h3>${data.title}</h3>
+    <p>${data.description}</p>
+    <p>${data.date}</p>`;
+
+    return sidebar;
+}
+
+mainElement.appendChild(createSidebar({
+  title: "Sidebar 1",
+  description: "Sidebar description 1",
+  date: "2021-08-06"
+}))
+
+// Page layout
+
+function modifyLayout(...elements) {
+  const mainElement = findElement("main");
+  mainElement.classList.add("layout");
+  elements.forEach((element) => {
+    const elementNode = findElement(element);
+    if (elementNode) {
+      elementNode.classList.add("highlight");
+    }
+  });
+}
+
+modifyLayout("main", "article", "aside");
