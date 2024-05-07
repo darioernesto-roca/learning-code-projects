d6cc01a5e0d422b721cbed9df9acd683d7cced5e
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-05-06 22:28:40 -0500

    Practicing event listeners

--- JavaScript/JS Training/Practice/09_04/script.js
+++ JavaScript/JS Training/Practice/09_04/script.js
@@ -5,3 +5,58 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+const gridContainer = document.querySelector(".grid");
+
+gridContainer.addEventListener("mouseenter", () => {
+    gridContainer.style.backgroundColor = "#dbdddd";
+}, false);
+
+gridContainer.addEventListener("mouseleave", () => {
+    gridContainer.style.backgroundColor = "";
+}, false);
+
+/**
+ * Function to generate random grey escale hex color
+ */
+
+const randomGrey = () => {
+    let greyColor = Math.floor(Math.random() * 256).toString(16);
+    return greyColor;
+}
+
+// Get all cells
+const gridCells = document.querySelectorAll(".cell");
+
+// For each cell, add eventlisteners aplenty
+gridCells.forEach((cell) => {
+    // Set outline when cell is hovered
+    cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = `#${randomGrey()}${randomGrey()}${randomGrey()}`;
+    }, false);
+
+    // Remove outline when cell is exited
+    cell.addEventListener("mouseleave", () => {
+        cell.style.backgroundColor = "";
+    }, false);
+
+    // Set/remove random background color on click
+    cell.addEventListener("click", () => {
+        if (cell.style.backgroundColor) {
+            cell.style.backgroundColor = "";
+        } else {
+            cell.style.backgroundColor = `#${randomGrey()}${randomGrey()}${randomGrey()}`;
+        }
+    }, false);
+});
+
+// Set page background using the "d" key on the keyboard
+const body = document.body;
+body.addEventListener("keydown", (event) => {
+    // event.code holds the current key pressed:
+    if (event.code === "KeyD") {
+        body.style.backgroundColor === ""
+        ? (body.style.backgroundColor = "hsl(201, 34%, 13%)")
+        : (body.style.backgroundColor = "");
+    }
+});
