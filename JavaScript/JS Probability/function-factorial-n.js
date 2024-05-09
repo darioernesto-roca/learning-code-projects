3eff7e14be8da85f311ab1fe893449b66f1bbd81
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-05-08 21:20:22 -0500

    Updated probability and text to Speech

--- JavaScript/JS Probability/function-factorial-n.js
+++ JavaScript/JS Probability/function-factorial-n.js
@@ -18,6 +18,5 @@ let totalCombinations = totalCombinationsArray1 * totalCombinationsArray2;
 
 let probability = 1 / totalCombinations;
 
-console.log(`The probability of the event is ${probability}`);
 let trials = 1 / probability;
-console.log(`The event is expected to occur once in every ${Math.round(trials)} trials.`);
+console.log(`La probabilidad de ganarte el baloto es de ${Math.ceil(probability)} en ${Math.round(trials)} intentos.`);
