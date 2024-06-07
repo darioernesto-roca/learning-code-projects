19e0260cb831d119ca1e017c1ba72eaa149738a5
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-06-06 23:49:30 -0500

    Python functions

--- python/functions.py
+++ python/functions.py
@@ -1,2 +1,17 @@
+# Functions
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
 def square(x):
     return x * x
+
+def cube(x):
+    return x * x * x
+
+def power(x, y):
+    return x ** y
+    
