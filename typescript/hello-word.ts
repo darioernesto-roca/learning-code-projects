77e244e4603b335028e06aaafe2a1235ebf34423
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-07-01 23:37:21 -0500

    Defining types in TypesCript

---
+++ typescript/hello-word.ts
@@ -0,0 +1,17 @@
+let helloWorld: string = "Hello, World!";
+
+console.log(helloWorld);  // Hello, World!
+
+// Interface declaration
+
+interface Client {
+    name: string,
+    id: 0
+}
+
+const client: Client = {
+    name: "Alice",
+    id: 0
+};
+
+console.log(client);  // { name: 'Alice', id: 0 }
