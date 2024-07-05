0c9b8bf23e8db2d87f478732c996491348e5ece2
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-07-04 22:35:38 -0500

    Python requests module example

---
+++ python/my_requests.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get('https://pokeapi.co/api/v2/pokemon/')
+
+data = response.json()
+
+# print(data)
+
+for pokemon in data['results']:
+    print(pokemon['name'])
