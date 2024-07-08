f12d9115e879e3edab2ab2094386c42e6f57b738
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-07-07 22:52:38 -0500

    Reverse a list in Python

--- python/lists.py
+++ python/lists.py
@@ -1,4 +1,4 @@
-# Flaten the lists
+# 1. Flatten the lists
 
 import itertools
 
@@ -6,3 +6,14 @@ a = [[1, 2], [3, 4], [5, 6]]
 b = list(itertools.chain.from_iterable(a))
 
 print(b)
+# Output: [1, 2, 3, 4, 5, 6]
+
+
+# Trick 2: Reverse a list
+
+a = [1, 2, 3, 4, 5]
+b = a[::-1]
+
+print(b)
+# Output: [5, 4, 3, 2, 1]
+
