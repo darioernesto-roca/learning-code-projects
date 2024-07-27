031ea2d1d0b05b8152ccb387d89d2b3cd528c0f8
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-07-26 23:13:32 -0500

    Destructuring

---
+++ JavaScript/JS destructuring/array-destructuring.js
@@ -0,0 +1,7 @@
+// Noobs:
+const array = [1, 2, 3];
+const first = array[0];
+const second = array[1];
+
+// Pro:
+const [first2, second2] = [1, 2, 3];
--- JavaScript/JS destructuring/destructuring.js
+++ JavaScript/JS destructuring/destructuring.js
@@ -7,3 +7,11 @@ const age = employee.age;
 const { name2, age2 } = employee;
 console.log(name2); // Output: Gary
 console.log(age2);  // Output: 28
+
+// Noobs:
+const user3 = { name: 'John', age: 30 };
+const name3 = name3.name;
+const age3 = age3.age;
+
+// Pro:
+const { name4, age4 } = { name: 'John', age: 30 };
