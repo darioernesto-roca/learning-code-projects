cda14151b189a51592146c9e7e140ca228e020c1
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-07-28 22:44:05 -0500

    Documentation about classes

--- python/classes.py
+++ python/classes.py
@@ -8,17 +8,44 @@ print(p.x)
 print(p.y)
 
 class Flight():
+    """
+    A class representing a flight.
+
+    Attributes:
+    - capacity (int): The maximum number of passengers the flight can accommodate.
+    - passengers (list): A list of passengers on the flight.
+
+    Methods:
+    - add_passenger(name): Adds a passenger to the flight.
+    - open_seats(): Returns the number of available seats on the flight.
+    """
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.passengers = []
 
     def add_passenger(self, name):
+        """
+        Adds a passenger to the flight.
+
+        Parameters:
+        - name (str): The name of the passenger.
+
+        Returns:
+        - bool: True if the passenger was successfully added, False otherwise.
+        """
         if not self.open_seats():
             return False
         self.passengers.append(name)
         return True
 
     def open_seats(self):
+        """
+        Returns the number of available seats on the flight.
+
+        Returns:
+        - int: The number of available seats.
+        """
         return self.capacity - len(self.passengers)
     
 flight = Flight(3)
