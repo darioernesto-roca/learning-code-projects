57e8b817e2676befec34bb0a02c31c1923ceb2d9
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-08-12 23:34:16 -0500

    Explain decorators and exceptions in Python

--- python/decorators.py
+++ python/decorators.py
@@ -1,3 +1,5 @@
+# Decorators are a way to modify or enhance the behavior of functions or methods. In this code snippet, we define a decorator called announce that prints a message before and after the function it decorates is called. We then apply this decorator to a function called hello, which prints "Hello, world!" when called. When we call hello, the message "About to run the function..." is printed before the function is called, and "Done with the function." is printed after the function is called.
+
 def announce(f):
     def wrapper():
         print("About to run the function...")
--- python/exceptions.py
+++ python/exceptions.py
@@ -1,3 +1,7 @@
+# To handle exceptions in Python we use the try and except blocks. The code that could potentially have an error is put in the try block. The program execution moves to the except block if an error happens. If there is no error, the code inside the except block will not run. We can also specify what type of error to catch and handle. This is done by adding an argument to the except block. In the example below, we catch a ValueError and a ZeroDivisionError. If the user enters a string instead of a number, a ValueError is raised. If the user tries to divide by 0, a ZeroDivisionError is raised. We also use the sys module to exit the program if an error occurs. This is done by calling the sys.exit() function and passing 1 as an argument. The argument is the exit code. An exit code of 0 means the program ran successfully. An exit code of 1 means an error occurred.
+
+# sys is a module that provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available.  
+
 import sys
 
 try:
