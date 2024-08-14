330011962c990b6b46d64a6ad34b426c6d75b974
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-08-13 23:45:26 -0500

    Typescript types and interfaces

--- typescript/variables.ts
+++ typescript/variables.ts
@@ -45,3 +45,43 @@ let notSure: any = 4;
 function warnUser(): void {
     console.log('This is my warning message');
 }
+
+// Number
+let num: number = 10;
+
+// String
+let str: string = 'Hello';
+
+// Array
+let arr: number[] = [1, 2, 3];
+
+// Tuple
+let tuple: [string, number] = ['Hello', 10];
+
+// Boolean
+let isTrue: boolean = true;
+
+// Object
+let obj: object = {name: 'John', age: 30};
+
+// Null
+let n: null = null;
+
+// Undefined
+let u: undefined = undefined;
+
+// Never: represents the type of values that never occur. It is used when a function never returns a value.
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// Interface
+interface Person {
+    name: string;
+    age: number;
+}
+
+let person: Person = {
+    name: 'John',
+    age: 30
+};
